adaff7f4d50422fff7fbc412566254f726a2d5c0
frank <frankmijhael01@gmail.com>
2026-08-01 15:22:47 -0500

    feat: add footer component

---
+++ frontend/energiai/src/app/shared/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class FooterComponent {
+  readonly currentYear = new Date().getFullYear();
+}
