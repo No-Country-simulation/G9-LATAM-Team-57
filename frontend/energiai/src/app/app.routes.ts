e68c498185728606ce95e345759e3090e2745964
frank <frankmijhael01@gmail.com>
2026-08-01 15:39:00 -0500

    feat: add routes for home and analysis

--- frontend/energiai/src/app/app.routes.ts
+++ frontend/energiai/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/home/home.routes').then((m) => m.HOME_ROUTES),
+  },
+  {
+    path: 'analysis',
+    loadChildren: () =>
+      import('./features/analysis/analysis.routes').then((m) => m.ANALYSIS_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
