1d789609dc043239be955eedb10fb0b0bfc6117a
frank <frankmijhael01@gmail.com>
2026-08-02 00:28:37 -0500

    feat: add design system and re build the components and form

--- frontend/energiai/src/app/features/analysis/pages/analysis-page/analysis-page.ts
+++ frontend/energiai/src/app/features/analysis/pages/analysis-page/analysis-page.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { EnergyForm } from '../../components/energy-form/energy-form';
+import { EnergyAnalysisRequest } from '../../models';
 
 @Component({
   selector: 'app-analysis-page',
-  imports: [],
+  standalone: true,
+  imports: [EnergyForm],
   templateUrl: './analysis-page.html',
   styleUrl: './analysis-page.scss',
 })
-export class AnalysisPage {}
+export class AnalysisPage {
+  onFormSubmit(request: EnergyAnalysisRequest): void {
+    // TODO: Integrar con EnergyAnalysisService en Sprint 3
+  }
+}
