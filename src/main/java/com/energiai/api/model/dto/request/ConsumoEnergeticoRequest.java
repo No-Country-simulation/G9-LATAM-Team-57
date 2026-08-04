40927f290e6d7eb9282e208ad827cab1fe545bd4
AgusL <lerda.m.agustina@gmail.com>
2026-08-04 12:20:59 -0300

    funcionalidad spring-api: record ConsumoEnergeticoRequest, DTO para request a la API python

--- src/main/java/com/energiai/api/model/dto/request/ConsumoEnergeticoRequest.java
+++ src/main/java/com/energiai/api/model/dto/request/ConsumoEnergeticoRequest.java
@@ -1,4 +1,16 @@
 package com.energiai.api.model.dto.request;
 
-public class ConsumoEnergeticoRequest {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ConsumoEnergeticoRequest (
+        @NotNull @PositiveOrZero Integer household_size,
+        @NotNull @PositiveOrZero Integer has_ac,
+        @NotNull Boolean home_office,
+        @NotNull HousingType housingType,
+        @NotNull @PositiveOrZero Integer equipment_count,
+        @NotNull @PositiveOrZero Double avg_energy_consumption_kwh,
+        @NotNull String peak_usage_level)
+
+{
 }
