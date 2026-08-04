20df946d57b2af76a3393da633f03589d6b80ef7
AgusL <lerda.m.agustina@gmail.com>
2026-08-03 23:59:25 -0300

    funcionalidad spring-api: enum HousingType del DTO

--- src/main/java/com/energiai/api/model/dto/request/HousingType.java
+++ src/main/java/com/energiai/api/model/dto/request/HousingType.java
@@ -1,8 +1,39 @@
 package com.energiai.api.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/** Mapea lo que ingresa el usuario por teclado
+ * */
 public enum HousingType {
     CASA,
     DEPARTAMENTO,
-    MONOAMBIENTE
+    MONOAMBIENTE;
 
-}
+
+
+    @JsonCreator
+    public static HousingType fromString(String valor){
+
+        if (valor ==null){
+            throw new IllegalArgumentException("El valor no puede ser nulo");
+        }
+        String normalizado = valor.trim().toUpperCase();
+
+        //Pruebo con el nombre del enum
+        for (HousingType tipo : values()){
+            if (tipo.name().equals(normalizado)){
+                return tipo;
+            }}
+
+        throw new IllegalArgumentException("El tipo de propiedad: "+ valor+" NO es reconocida.");
+     }
+
+     @JsonValue
+     public  String toValue(){
+        return this.name();
+    }
+
+    }
+
+
