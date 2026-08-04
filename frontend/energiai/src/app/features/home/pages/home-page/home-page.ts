30080bea3e8460e477ddba024ef42fa503ba3b42
frank <frankmijhael01@gmail.com>
2026-08-03 22:17:09 -0500

    feat: landing page

--- frontend/energiai/src/app/features/home/pages/home-page/home-page.ts
+++ frontend/energiai/src/app/features/home/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, MatIconModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
